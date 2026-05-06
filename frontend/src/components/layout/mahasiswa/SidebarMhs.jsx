f853a2443a074fe5a4a4083be4a6e64b6c82fd21
Fatiyya Ilmi <fatiyyailmi@gmail.com>
2026-05-07 02:00:12 +0700

    feat: update sidebar mahasiswa

--- frontend/src/components/layout/mahasiswa/SidebarMhs.jsx
+++ frontend/src/components/layout/mahasiswa/SidebarMhs.jsx
@@ -17,7 +17,7 @@ const SidebarMhs = () => {
         <aside className="w-64 min-h-screen bg-indigo-900 text-white flex flex-col">
             
             {/* Logo */}
-            <div className="p-6 border-b border-indigo-700">
+            <div className="p-6 border-b border-indigo-500">
                 <h1 className="text-2xl font-bold">ICON</h1>
                 <p className="text-sm text-yellow-300">
                     IPB Career Opportunity Network
@@ -85,7 +85,7 @@ const SidebarMhs = () => {
             </nav>
 
             {/* SETTINGS */}
-            <div className="p-4 border-t border-indigo-700">
+            <div className="p-4 border-t border-indigo-500">
                 <button className="flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-indigo-700 w-full">
                 <Settings size={20} />
                 <span>Pengaturan</span>
